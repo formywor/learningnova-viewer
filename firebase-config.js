5667ec58cc460b8b9e2c9af44739c3e726a3b5bb
formywor <nidalwonderofficialyt@gmail.com>
2025-10-27 22:27:06 +0000

    10101011111

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// learningnova.site / firebase-config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyCSj-wX066dbIuc_hO4Clt4uz5poAVayp4",
+  authDomain: "learningnova.firebaseapp.com",
+  projectId: "learningnova",
+  storageBucket: "learningnova.appspot.com",
+  messagingSenderId: "505366608345",
+  appId: "1:505366608345:web:7abde83978ea5ae66ec1c8",
+  measurementId: "G-6KH2YXJZ63"
+};
